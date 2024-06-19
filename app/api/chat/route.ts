b8b8695acfde1513b60d9919e0bacf0001432fb5
GargDivanshu <divanshu.garg113@gmail.com>
2024-06-19 13:55:19 +0530

    edge function again made

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -10,7 +10,7 @@ const config = new Configuration({
 });
 const openai = new OpenAIApi(config);
 
-// export const runtime = "edge";
+export const runtime = "edge";
 
 export async function POST(req: Request) {
   try {
